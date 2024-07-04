8e830632593c0df172fa985f441ecbd907cf50cf
Marietta7747 <162296008+Marietta7747@users.noreply.github.com>
2024-07-04 10:47:44 +0200

    Add files via upload

---
+++ diakkezeloB/diakkezeloB/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace diakkezeloB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlEvek = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstDiakok = new System.Windows.Forms.ListBox();
+            this.btnAdatbevitel = new System.Windows.Forms.Button();
+            this.lstEredmeny = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDiak = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // pnlEvek
+            // 
+            this.pnlEvek.AutoScroll = true;
+            this.pnlEvek.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.pnlEvek.Location = new System.Drawing.Point(297, 81);
+            this.pnlEvek.Name = "pnlEvek";
+            this.pnlEvek.Size = new System.Drawing.Size(350, 68);
+            this.pnlEvek.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(95, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Diákok";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(408, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Születési év";
+            // 
+            // lstDiakok
+            // 
+            this.lstDiakok.FormattingEnabled = true;
+            this.lstDiakok.Location = new System.Drawing.Point(32, 81);
+            this.lstDiakok.Name = "lstDiakok";
+            this.lstDiakok.Size = new System.Drawing.Size(211, 303);
+            this.lstDiakok.TabIndex = 6;
+            // 
+            // btnAdatbevitel
+            // 
+            this.btnAdatbevitel.Location = new System.Drawing.Point(100, 410);
+            this.btnAdatbevitel.Name = "btnAdatbevitel";
+            this.btnAdatbevitel.Size = new System.Drawing.Size(75, 23);
+            this.btnAdatbevitel.TabIndex = 7;
+            this.btnAdatbevitel.Text = "Adatbevitel";
+            this.btnAdatbevitel.UseVisualStyleBackColor = true;
+            this.btnAdatbevitel.Click += new System.EventHandler(this.btnAdatbevitel_Click);
+            // 
+            // lstEredmeny
+            // 
+            this.lstEredmeny.FormattingEnabled = true;
+            this.lstEredmeny.Location = new System.Drawing.Point(297, 200);
+            this.lstEredmeny.Name = "lstEredmeny";
+            this.lstEredmeny.Size = new System.Drawing.Size(350, 186);
+            this.lstEredmeny.TabIndex = 8;
+            this.lstEredmeny.SelectedIndexChanged += new System.EventHandler(this.lstEredmeny_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(297, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "A keresés eredménye:";
+            // 
+            // lblDiak
+            // 
+            this.lblDiak.AutoSize = true;
+            this.lblDiak.Location = new System.Drawing.Point(297, 419);
+            this.lblDiak.Name = "lblDiak";
+            this.lblDiak.Size = new System.Drawing.Size(0, 13);
+            this.lblDiak.TabIndex = 10;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.lblDiak);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstEredmeny);
+            this.Controls.Add(this.btnAdatbevitel);
+            this.Controls.Add(this.lstDiakok);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlEvek);
+            this.Name = "Form1";
+            this.Text = "Diákok adatai";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlEvek;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstDiakok;
+        private System.Windows.Forms.Button btnAdatbevitel;
+        private System.Windows.Forms.ListBox lstEredmeny;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDiak;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ diakkezeloB/diakkezeloB/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace diakkezeloB
+{
+    public partial class Form1 : Form
+    {
+        private List<int> evek = new List<int>();
+        private List<Button> btnEvek = new List<Button>();  
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog1.FileName = "";
+        }
+
+        private void AdatBevitel()
+        {
+            DialogResult eredmeny = openFileDialog1.ShowDialog();
+            if (eredmeny == DialogResult.OK)
+            {
+                string fajlNev = openFileDialog1.FileName;
+                try
+                {
+                    AdatBeolvasas(fajlNev);
+                    FelrakEvek();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Hiba a fájl beolvasásakor", "Hiba");
+                }
+            }
+        }
+
+        private void Feldolgoz(string adat)
+        {
+            string[] adatok = adat.Split(';');
+            Diak diak;
+            diak = new Diak(adatok[0], adatok[1], int.Parse(adatok[2]));
+            lstDiakok.Items.Add(diak);
+            if (!evek.Contains(diak.SzulEv)) evek.Add(diak.SzulEv);
+        }
+
+        private void AdatBeolvasas(string fajlNev)
+        {
+            string adat;
+            StreamReader sr = new StreamReader(fajlNev);
+
+            while (!sr.EndOfStream)
+            {
+                adat = sr.ReadLine();
+                Feldolgoz(adat);
+            }
+            sr.Close();
+        }
+        private int kezdoX = 10, btnXKoz = 80, kezdoY = 15;
+        private void FelrakEvek()
+        {
+            Button btn;
+            evek.Sort();
+            for (int i = 0; i < evek.Count; i++)
+            {
+                btn = new Button();
+                btn.Location = new Point(kezdoX + i * btnXKoz, kezdoY);
+                btn.Text = evek[i].ToString();
+
+                btn.Click += new System.EventHandler(Kivalaszt);
+                
+                //Rárakjuk a panelre a gombot
+                pnlEvek.Controls.Add(btn);
+
+                //Hozzáadjuk a gombok listájához
+                btnEvek.Add(btn);
+            }
+        }
+
+        private void btnAdatbevitel_Click(object sender, EventArgs e)
+        {
+            AdatBevitel();
+            btnAdatbevitel.Enabled = false;
+        }
+
+        private void Kivalaszt(object sender, EventArgs e)
+        {
+            //Az év értéke az aktuálisan megnyomott gomb felirata
+            int ev = int.Parse((sender as Button).Text);
+
+            lstEredmeny.Items.Clear();
+            foreach(Diak diak in lstDiakok.Items)
+            {
+                if (diak.SzulEv == ev) lstEredmeny.Items.Add(diak);
+            }
+        }
+
+        private void lstEredmeny_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Diak diak = (Diak)lstEredmeny.SelectedItem;
+            if (diak != null) lblDiak.Text = diak + ", születési éve: " + diak.SzulEv;
+        }
+    }
+    class Diak
+    {
+        public string Nev { get; private set; }
+        public string TanulKod { get; private set; }
+
+        public int SzulEv { get; private set; }
+
+        public Diak(string nev, string tanulKod, int szulEv)
+        {
+            this.Nev = nev;
+            this.TanulKod = tanulKod;
+            this.SzulEv = szulEv;
+        }
+
+        public override string ToString()
+        {
+            return Nev + " (" + TanulKod + ")";
+        }
+    }
+}
