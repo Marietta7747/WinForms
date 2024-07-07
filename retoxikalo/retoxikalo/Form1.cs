a358029d1ff4968898b26f3666cc5a3e3addafb4
Marietta7747 <162296008+Marietta7747@users.noreply.github.com>
2024-07-07 08:51:59 +0200

    Add files via upload

--- retoxikalo/retoxikalo/Form1.cs
+++ retoxikalo/retoxikalo/Form1.cs
@@ -63,6 +63,9 @@ namespace retoxikalo
 
                     ItallapMenuItem.Enabled = true;
                     SaveMenuItem.Enabled = true;
+
+                    italForm.ArlapotIr(italok);
+                    italForm.Show();
                 }
                 catch (Exception ex)
                 {
@@ -89,16 +92,53 @@ namespace retoxikalo
             }
         }
 
+        
     }
     class Ital
     {
         public string ItalNev { get; private set; }
         public int EgysegAr { get; set; }
+        public int Mennyiseg { get; private set; }
+        public int OsszMennyiseg {  get; private set; }
+        public string Bevetel {  get; private set; }
 
         public Ital(string italNev, int egysegAr)
         {
             ItalNev = italNev;
             EgysegAr = egysegAr;
+        }
+
+        public void Rendel(int db)
+        {
+            Mennyiseg += db;
+        }
+
+        public int Fizetendo()
+        {
+            return Mennyiseg * EgysegAr;
+        }
+
+        public void Fizet()
+        {
+            OsszMennyiseg += Mennyiseg;
+            Bevetel += Mennyiseg * EgysegAr;
+            Mennyiseg = 0;
+        }
+
+        public string Arlistaba()
+        {
+            return ItalNev + " (" + EgysegAr + " Ft)";
+        }
+
+        public string Konyvelesbe()
+        {
+            return ItalNev + ";" + OsszMennyiseg + ";" + Bevetel;
+        }
+
+        public override string ToString()
+        {
+            return Mennyiseg.ToString().PadLeft(4) + " " + ItalNev.PadRight(35) + 
+                Fizetendo().ToString().PadLeft(10) + " Ft";
         }
     }
 
--- retoxikalo/retoxikalo/ItalLapForm.cs
+++ retoxikalo/retoxikalo/ItalLapForm.cs
@@ -16,5 +16,59 @@ namespace retoxikalo
         {
             InitializeComponent();
         }
+
+        private List<Ital> italok = new List<Ital>();
+        private List<CheckBox> chkBoxok = new List<CheckBox>();
+        private List<TextBox> txtBoxok = new List<TextBox>();
+
+        private int bal = 10, fent = 10, chkXMeret = 250, chkYTav = 40,
+            txtXMeret = 30, txtYmeret = 17, xKoz = 5;
+
+        private int maxAdag = 999;
+
+        internal void ArlapotIr(List<Ital> italok)
+        {
+            pnlValasztek.Controls.Clear();
+            chkBoxok.Clear();
+            txtBoxok.Clear();
+
+            this.italok = italok;
+
+            CheckBox chkBox;
+            TextBox txtBox;
+            Label lbl;
+
+            for (int i = 0; i < italok.Count; i++)
+            {
+                chkBox = new CheckBox();
+                txtBox = new TextBox();
+                lbl = new Label();
+
+                //chkBox
+                chkBox.Location = new Point(bal, fent + i * chkYTav);
+                chkBox.Size = new Size(chkXMeret, txtYmeret);
+                chkBox.Text = italok[i].Arlistaba();
+                chkBox.TextAlign = ContentAlignment.MiddleLeft;
+
+                //txtBox
+                txtBox.Location = new Point(bal + chkXMeret, fent + i * chkYTav - 2);
+                txtBox.Size = new Size(txtXMeret, txtYmeret);
+
+                //lbl
+                lbl.AutoSize = true;
+                lbl.Location = new Point(bal + chkXMeret + txtXMeret + xKoz,
+                                        fent + i * chkYTav + 1);
+                lbl.Text = "adag";
+
+                //panelre rakás
+                pnlValasztek.Controls.Add(chkBox);
+                pnlValasztek.Controls.Add(txtBox);
+                pnlValasztek.Controls.Add(lbl);
+
+                //listákba pakolás
+                chkBoxok.Add(chkBox);
+                txtBoxok.Add(txtBox);
+            }
+        }
     }
 }
